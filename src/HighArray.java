71e8419ec42feccc7075be06764dc0f80d31efd2
Vladlen Astakhov <vladlenastakhov@gmail.com>
2020-06-24 12:12:19 +0300

    1.1 Added getMax method
    Signed-off-by: Vladlen Astakhov <vladlenastakhov@gmail.com>

--- src/HighArray.java
+++ src/HighArray.java
@@ -1,47 +1,60 @@
 class HighArray {
-    private long[] a;                 // ref to array a
-    private int nElems;               // number of data items
+    private int[] array;                 // ref to array a
+    private int numberOfElementsInArray;               // number of data items
 
     public HighArray(int max){        // constructor
-        a = new long[max];                 // create the array
-        nElems = 0;                        // no items yet
+        array = new int[max];                 // create the array
+        numberOfElementsInArray = 0;                        // no items yet
     }
 
     public boolean find(long searchKey){   // find specified value
         int j;
-        for(j=0; j<nElems; j++)            // for each element,
-            if(a[j] == searchKey)           // found item?
+        for(j=0; j<numberOfElementsInArray; j++)            // for each element,
+            if(array[j] == searchKey)           // found item?
                 break;                       // exit loop before end
-        if(j == nElems)                    // gone to end?
+        if(j == numberOfElementsInArray)                    // gone to end?
             return false;                   // yes, can't find it
         else
             return true;                    // no, found it
     }  // end find()
 
-    public void insert(long value){    // put element into array
-        a[nElems] = value;             // insert it
-        nElems++;                      // increment size
+    public void insert(int value){    // put element into array
+        array[numberOfElementsInArray] = value;             // insert it
+        numberOfElementsInArray++;                      // increment size
     }
 
     public boolean delete(long value){
         int j;
-        for(j=0; j<nElems; j++)        // look for it
-            if(value == a[j] )
+        for(j=0; j<numberOfElementsInArray; j++)        // look for it
+            if(value == array[j] )
                 break;
-        if(j==nElems)                  // can't find it
+        if(j== numberOfElementsInArray)                  // can't find it
             return false;
         else{                           // found it
-            for(int k=j; k<nElems; k++) // move higher ones down
-                a[k] = a[k+1];
-            nElems--;                   // decrement size
+            for(int k = j; k<numberOfElementsInArray; k++) // move higher ones down
+                array[k] = array[k+1];
+            numberOfElementsInArray--;                   // decrement size
             return true;
         }
     }  // end delete()
 
     public void display(){           // displays array contents
-        for(int j=0; j<nElems; j++)       // for each element,
-            System.out.print(a[j] + " ");  // display it
+        for(int j = 0; j< numberOfElementsInArray; j++)       // for each element,
+            System.out.print(array[j] + " ");  // display it
         System.out.println("");
+    }
+
+    public int getMax(){
+        int maxNumber = 0;
+        if (numberOfElementsInArray == 0) {
+            System.out.println("Array is empty");
+        }
+        else {
+            for (int x:array) {
+                if (x > maxNumber) maxNumber = x;
+            }
+        }
+        return maxNumber;
     }
 
 }  // end class HighArray
@@ -56,7 +69,7 @@ class HighArrayApp {
         arr.insert(77);               // insert 10 items
         arr.insert(99);
         arr.insert(44);
-        arr.insert(55);
+        arr.insert(160);
         arr.insert(22);
         arr.insert(88);
         arr.insert(11);
@@ -67,15 +80,14 @@ class HighArrayApp {
         arr.display();                // display items
 
         int searchKey = 35;           // search for item
-        if( arr.find(searchKey) )
-            System.out.println("Found " + searchKey);
-        else
-            System.out.println("Can't find " + searchKey);
+        if(arr.find(searchKey)) System.out.println("Found " + searchKey);
+        else System.out.println("Can't find " + searchKey);
 
         arr.delete(0);               // delete 3 items
         arr.delete(55);
         arr.delete(99);
 
         arr.display();                // display items again
+        System.out.println("Max number in an array is: " + arr.getMax());
     }  // end main()
 }  // end class HighArrayApp
